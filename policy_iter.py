add692050f5a54e04ae22b279fa91fc6948655ae
Mich8952 <michael.murray8952@gmail.com>
2025-11-13 21:54:02 -0500

    bug fix

--- policy_iter.py
+++ policy_iter.py
@@ -41,7 +41,8 @@ class PolicyItr:
                 pi[tuple(state[0])] = random.choice(possible_actions).item()
         return pi
         
-    def eval(self, pi, V, epsilon=1e-4, gamma=0.9):
+    def eval(self, pi_X, pi_O, V, epsilon=1e-4, gamma=0.9):
+        
         for state in self.all_states:
             if state[1]:
                 env = TicTacToeEnv(self.n)
@@ -58,30 +59,30 @@ class PolicyItr:
             
             for state in self.all_states:
                 if state[1]:  
-                    continue # the value is zero and continues to be zero so just keep going (ignore the below)
+                    continue 
                     
                 state_tuple = tuple(state[0])
                 env = TicTacToeEnv(self.n)
                 env.state = np.array(state[0]).reshape((self.n, self.n))
                 env.set_player_auto()
                 
-                action = pi[state_tuple] # get next action
+                
+                if env.current_player == 1:  # Xs turn
+                    action = pi_X[state_tuple]
+                else:  # y s turn
+                    action = pi_O[state_tuple]
                 
                 env_copy = deepcopy(env)
                 env_copy.step(action)
-                next_state_tuple = tuple(env_copy.get_flat_state()) # next state after taking that action
-                
+                next_state_tuple = tuple(env_copy.get_flat_state())
                 
                 reward = env_copy.check_game_status()
-                if reward is None: # not terminated
+                if reward is None:
                     reward = 0.0
                 else:
-                    reward = float(reward) # terminated game
+                    reward = float(reward)
                 
-                
-                
-                V_new[state_tuple] = reward + gamma* V[next_state_tuple]
-                
+                V_new[state_tuple] = reward + gamma * V[next_state_tuple]
                 
                 delta = max(delta, abs(V_new[state_tuple] - V[state_tuple]))
             
@@ -93,14 +94,17 @@ class PolicyItr:
         
         return V 
     
-    def improve(self,V, gamma= 0.9):
-        pi_star = {}
+    def improve(self, V, gamma=0.9):
+        pi_X = {}
+        pi_O = {}
+        
         for state in self.all_states:
-            if state[1]: # terminal skipp
+            if state[1]:  
                 continue
+                
             env = TicTacToeEnv(self.n)
             env.state = np.array(state[0]).reshape((self.n, self.n))
-            env.set_player_auto()  
+            env.set_player_auto()
 
             possible_actions = env.get_possible_actions()
 
@@ -115,46 +119,55 @@ class PolicyItr:
                 next_state_tuple = tuple(clone_env.get_flat_state())
 
                 if rwd is None:
-                    rwd = 0.0 # recheck because this is also a draw, but we dont want to return none
+                    rwd = 0.0
                 else:
                     rwd = float(rwd)
                     
-                q = rwd + gamma*V[next_state_tuple]
+                q = rwd + gamma * V[next_state_tuple]
                 action_values.append(q)
-                    
-            if env.current_player == 1:  # X player
-                best_idx = np.argmax(action_values)
-            else:  # y player
-                best_idx = np.argmin(action_values)  
             
-            pi_star[tuple(state[0])] = possible_actions[best_idx].item()
+            
+            if env.current_player == 1:  # X player maximizes
+                best_idx = np.argmax(action_values)
+                pi_X[tuple(state[0])] = possible_actions[best_idx].item()
+            else:  # y player minimizes
+                best_idx = np.argmin(action_values)
+                pi_O[tuple(state[0])] = possible_actions[best_idx].item()
   
-        return pi_star
+        return pi_X, pi_O
             
         
             
     def loop(self, max_iters=100):
-        pi = {}
-        pi_bar = {}
+        pi_X = PolicyItr.set_policy_random(None, self.all_states, self.n)
+        pi_O = PolicyItr.set_policy_random(None, self.all_states, self.n)
         
-        pi_bar = PolicyItr.set_policy_random(pi_bar, self.all_states,self.n)
-        pi = PolicyItr.set_policy_random(pi, self.all_states,self.n)
+        pi_X_bar = PolicyItr.set_policy_random(None, self.all_states, self.n)
+        pi_O_bar = PolicyItr.set_policy_random(None, self.all_states, self.n)
+        
         V = {tuple(state[0]): 0.0 if state[1] else random.random() for state in self.all_states}
 
-        i =0 
-        while pi != pi_bar and i < max_iters:
-            V = self.eval(pi, V)
-            pi = deepcopy(pi_bar) # weird lecture thing
-            pi_bar = self.improve(V)
-            i+=1
-
+        i = 0
+        while (pi_X != pi_X_bar or pi_O != pi_O_bar) and i < max_iters:
+            
+            V = self.eval(pi_X, pi_O, V)
+            
+            
+            pi_X = deepcopy(pi_X_bar)
+            pi_O = deepcopy(pi_O_bar)
+            
+            
+            pi_X_bar, pi_O_bar = self.improve(V)
+            
+            i += 1
             if i % 10 == 0:
-                print(i)
+                print(f"Iteration {i}")
 
-        return pi_bar, V
+        return pi_X_bar, pi_O_bar, V
 
 
 if __name__ == "__main__":
     piter = PolicyItr(n=3)
-    optimal_policy, optimal_values = piter.loop()  
+    pi_X, pi_O, V = piter.loop()  
+
     print("EOF")
