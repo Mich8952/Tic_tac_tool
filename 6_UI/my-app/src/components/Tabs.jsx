591fa92233d65f302816be0beab90b9c7eef2d90
C F <charlotte.fritz@mail.utoronto.ca>
2025-12-07 15:45:17 -0500

    added readme

--- 6_UI/my-app/src/components/Tabs.jsx
+++ 6_UI/my-app/src/components/Tabs.jsx
@@ -3858,11 +3858,11 @@ export default function Tabs() {
         };
       } else if (plotIndex === 5) {
         return {
-          labels: montecarloResults.algorithm, // x-axis labels
+          labels: montecarloResults.algorithms, // x-axis labels
           datasets: [
             {
               label: "Wins",
-              data: montecarloResults.algs_4x4.win_rates, // height of "Wins" segment
+              data: montecarloResults.algs_4x4.win_rates,
               backgroundColor: "rgba(46, 204, 113, 0.7)",
               stack: "Stack 0", // same stack group
             },
---
+++ README.md
@@ -0,0 +1,24 @@
+# Tic Tac Tool
+
+## Getting Started
+
+# Download the policies from the Sharepoint
+
+# Clone the repository
+
+git clone https://github.com/Mich8952/Tic_tac_tool.git
+
+# Navigate into the project
+
+cd Tic_tac_tool/
+
+# Place the downloaded folder from sharepoint into the directory
+
+# Navigate to the following director
+
+cd Tic_tac_tool/6_UI/my-app
+
+# Install dependencies
+
+npm install
+npm run start
