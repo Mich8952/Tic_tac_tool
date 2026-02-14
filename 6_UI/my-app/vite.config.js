53e0cb83b94d9fa43e7140430a857c5c2fd012b1
Tomasdfgh <tomnguyenvn63@gmail.com>
2026-02-14 17:59:39 -0500

    Dockerized for prod environment

--- 6_UI/my-app/vite.config.js
+++ 6_UI/my-app/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['tictactool.duckdns.org'],
+    allowedHosts: ['tictactool.tom-nguyen.ca'],
   },
 })
